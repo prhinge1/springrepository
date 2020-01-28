89cb1779ebb3c6ffc934a3f564d049dafef6df97
Prasanna, Hinge <impdh@rediffmail.com>
2020-01-28 15:33:26 +0530

    New project
    New project file checked in repository

---
+++ Webdemo/src/main/java/com/example/webdemo/WebdemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.webdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class WebdemoApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(WebdemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdemoApplication.class, args);
+	}
+}
